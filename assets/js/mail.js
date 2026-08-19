7b7b2168739279149f913a1228d820410d063653
Satyarth Sharma <118390005+shsat94@users.noreply.github.com>
2026-08-19 13:47:17 +0530

    Update sendmail function to simplify parameters
    Removed subject and budget fields from email parameters.

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -2,8 +2,6 @@ function sendmail() {
     let params = {
         name: document.getElementById("fname").value,
         email: document.getElementById("femail").value,
-        subject: document.getElementById("fsubject").value,
-        budget: document.getElementById("fbudget").value,
         comment: document.getElementById("fcomment").value
     }
 
