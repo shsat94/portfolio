221b42db1635f42c4e6e9756357e9c58714f279a
Satyarth Sharma <118390005+shsat94@users.noreply.github.com>
2026-08-19 13:58:17 +0530

    Update email service ID in mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -5,5 +5,5 @@ function sendmail() {
         comment: document.getElementById("fcomment").value
     }
 
-    emailjs.send("service_9ek743o","template_xz0k985",params).then(alert("Email is sent"));
+    emailjs.send("service_uj30i5r","template_xz0k985",params).then(alert("Email is sent"));
 }
